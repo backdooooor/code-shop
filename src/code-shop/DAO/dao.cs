f5c4d8f66aeed0fb5d2fa042d9d531a43a84b733
backdoor <spam@mir-saratov.ru>
2011-01-05 20:29:32 +0300

    здесь нужно изменить строку подключения а то ничего работать не будет

---
+++ src/code-shop/DAO/dao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace code_shop.DAO
+{
+    public class dao
+    {
+        private string constring;
+        public dao()
+        {
+
+        }
+    }
+}
