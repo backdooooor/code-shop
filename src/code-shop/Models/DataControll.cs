c47ca59e23a8064ad308bb6f56feb47216ef0ed7
backdoor <spam@mir-saratov.ru>
2011-01-05 20:36:10 +0300

    нужно изменить DataString в возвращаемом методе StringConnect а то ничего работать не будет

---
+++ src/code-shop/Models/DataControll.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace code_shop.Models
+{
+    public class DataControll
+    {
+        public static String StringConnect()
+        {
+            return @"Data Source=.\SQLEXPRESS;AttachDbFilename=H:\code-shop\code-shop\App_Data\codeshop.mdf;Integrated Security=True;User Instance=True";
+        }
+    }
+}
